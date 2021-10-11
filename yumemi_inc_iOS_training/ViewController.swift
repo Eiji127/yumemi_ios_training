3b2a45b29263dbba843499c2293d12e19cba7bf5
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-10-11 15:57:38 +0900

    [Update]: give some constraints for some components.

--- yumemi_inc_iOS_training/ViewController.swift
+++ yumemi_inc_iOS_training/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var minimumTemperatureLabel: UILabel!
+    @IBOutlet weak var maximumTemperatureLabel: UILabel!
+    
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var reloadButton: UIButton!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
 
 
